0709bdc59b42c750666d63635e1445b05a4512da
Dmitrian23 <dimonio1991@mail.ru>
2023-02-08 23:32:46 +0300

    Задача 031

---
+++ 031/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача_№31 Вывести на экран кубы чисел от 1 до N
+
+int i=1;
+int N,S;
+
+       System.Console.Write("Введите пожалуйста число для переменной(N):");
+            N=Convert.ToInt32(Console.ReadLine());
+            
+   if(N<=0)
+     {
+       System.Console.WriteLine("___Error___Вы вводите неверное значение!");  
+       System.Console.WriteLine(" Число для переменной N должно быть >1");  
+     }
+while(i<=N)
+     {
+      S=i*i*i;   
+       System.Console.WriteLine($"Куб числа ------> {i} это {S}");
+      i=i+1;
+     }
+
+
+
+
+
+
+
