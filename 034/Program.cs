e608dda6ce5ee2abb6d100361538ee9ddd77f880
Dmitrian23 <dimonio1991@mail.ru>
2023-02-08 23:33:32 +0300

    Задача 034

---
+++ 034/Program.cs
@@ -0,0 +1,26 @@
+﻿// Зфдача_№34 Определить количество цифр в числе. Сделать подпрограмму.
+
+int S;
+
+          System.Console.Write("Введите пожалуйста число__:");
+               S=Convert.ToInt32(Console.ReadLine());
+if(S<=0)
+  {
+          System.Console.WriteLine("_______________E_R_R_O_R_______________");
+          System.Console.WriteLine("Введите многозначное число больше нуля!");
+  }
+else
+  {
+          System.Console.WriteLine($"Количество цифр в числе____{CounterDigits(S)}");
+
+int CounterDigits(int S)
+ {
+   int counter=0;
+   while(S!=0)
+      {
+       counter++;
+       S/=10;
+      }
+  return counter;
+ }
+  }   
